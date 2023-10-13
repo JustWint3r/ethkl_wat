c90c736b415a6853029259fdc8339bd682ca3cb8
TeeHaoBin <125014451+TeeHaoBin@users.noreply.github.com>
2023-10-13 18:07:46 +0000

    Failed but maybe useful

--- create-wallet/src/App.js
+++ create-wallet/src/App.js
@@ -1,20 +1,35 @@
 import {ethers} from 'ethers';
-
+import { useState } from 'react';
 
 function App() {
-  
+  const [wallet, setWallet] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+
+  // Check if wallet is installed and enabled
+  const isWalletInstalled = window.ethereum && window.ethereum.isMetaMask;
+
   // Connect wallet
   const connectWallet = async () => {
-    const wallet = new ethers.BrowserProvider(window.ethereum);
-    const accounts = await wallet.listAccounts();
-    console.log(accounts);
-  }
+    if (!isWalletInstalled) {
+      throw new Error('Please install and enable MetaMask');
+    }
 
-  connectWallet();
+    try {
+      const newWallet = new ethers.BrowserProvider(window.ethereum);
+      setWallet(newWallet);
+      const newAccounts = await newWallet.listAccounts();
+      setAccounts(newAccounts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="App">
       <button onClick={connectWallet}>Connect Wallet</button>
+      {wallet && accounts.length > 0 && (
+        <p>Connected to account: {accounts[0]}</p>
+      )}
     </div>
   );
 }
