b4608aa54f9683d6eae539d793c44c986650c2af
TeeHaoBin <125014451+TeeHaoBin@users.noreply.github.com>
2023-10-14 22:53:55 +0800

    time lock nft feature with js and sol file

---
+++ time-lock-nft/abi.js
@@ -0,0 +1,59 @@
+export const abi = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "desiredTimestamp",
+                "type": "uint256"
+            }
+        ],
+        "name": "setUnlockTimestamp",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "unlockTimestamp",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
--- time-lock-nft/src/App.js
+++ time-lock-nft/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers'; // You'll need ethers.js or web3.js for Ethereum interaction
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const NFTTimeLockForm = ({ contractAddress, abi, signer }) => {
+    const [unlockTimestamp, setUnlockTimestamp] = useState('');
+
+    const handleUnlockTimestampChange = (event) => {
+        setUnlockTimestamp(event.target.value);
+    };
+
+    const handleSetUnlockTimestamp = async () => {
+        try {
+            // Convert the date and time input to a Unix timestamp (in seconds)
+            const desiredTimestamp = Math.floor(Date.parse(unlockTimestamp) / 1000);
+
+            // Connect to the smart contract
+            const contract = new ethers.Contract(contractAddress, abi, signer);
+
+            // Call the setUnlockTimestamp function
+            const tx = await contract.setUnlockTimestamp(desiredTimestamp);
+            await tx.wait();
+
+            alert('Unlock timestamp set successfully!');
+        } catch (error) {
+            console.error('Error setting unlock timestamp:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Set Unlock Timestamp for NFT</h1>
+            <input
+                type="datetime-local"
+                value={unlockTimestamp}
+                onChange={handleUnlockTimestampChange}
+            />
+            <button onClick={handleSetUnlockTimestamp}>Set Unlock Timestamp</button>
+        </div>
+    );
+};
+
+export default NFTTimeLockForm;
