45d6b2e716577efd99efa3f07bfa2958a03ccc24
Wint3r- <145309650+JustWint3r@users.noreply.github.com>
2023-10-14 23:42:10 +0800

    main menu 90% done

--- ethertix/src/App.js
+++ ethertix/src/App.js
@@ -21,13 +21,37 @@ function App() {
   return (
    
     <div class="main-menu">
+    <div class="ocean">
+  <div class="wave"></div>
+  <div class="wave"></div>
+</div>
     <img src={logo} alt="EtherTix Logo" />
     <h1 class="title">EtherTix</h1>
     <p class="slogan">Your ticket, your NFT, your experience.</p>
     <div class="buttons">
-        <button class="connect-wallet">Connect Wallet</button>
-        <button class="buy-tickets">Buy Tickets</button>
-        <button class="view-history">View History</button>
+        <button class="button">
+          Connect Wallet
+        <div class ="hoverEffect">
+          <div>
+          </div>
+        </div></button>
+        <button class="button">
+          Buy Tickets
+        <div class="hoverEffect">
+          <div>
+          </div>
+        </div></button>
+        <button class="button">
+          View History
+        <div class="hoverEffect">
+          <div>
+          </div>
+        </div></button>
+
+<div class="hoverEffect">
+<div>
+</div>
+</div>
     </div>
   </div>
   );
