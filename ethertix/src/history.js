b51c79ea7f8aaa0b53fc9b04695b70046c9efaa9
TeeHaoBin <125014451+TeeHaoBin@users.noreply.github.com>
2023-10-15 00:44:26 +0800

    testing any missing files

---
+++ ethertix/src/history.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Subpage() {
+  return (
+    <div>
+      <h1>Past Transactions</h1>
+      {/* Add your history content here */}
+    </div>
+  );
+}
+
+export default Subpage;
